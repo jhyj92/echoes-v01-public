0a176ec25cbd90601ebbf9424021e16f91f7085e
jhyj92 <bloodpiru@gmail.com>
2025-04-28 09:36:14 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: false, // 👈 DISABLE App Router to use Pages Router correctly
-  },
 };
 
 module.exports = nextConfig;
