a662f0f51f5a3887aea7cb5d5cd51baff6b93fcd
jhyj92 <bloodpiru@gmail.com>
2025-04-29 20:55:41 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@components": require("path").resolve(__dirname, "components"),
+      "@utils": require("path").resolve(__dirname, "utils"),
+      "@hooks": require("path").resolve(__dirname, "hooks"),
+      "@styles": require("path").resolve(__dirname, "styles")
+    };
+    return config;
+  }
 };
 module.exports = nextConfig;
