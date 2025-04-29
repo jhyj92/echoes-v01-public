dde0526922c117d26f4b000a2434ea197baf98be
jhyj92 <bloodpiru@gmail.com>
2025-04-29 21:01:57 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 // pages/_app.js
-"use client";
+import "@styles/themes.css";   // global theme
+import "@styles/style.css";    // base styles (rename path)
 
-import "../styles/themes.css";   // <-- only global import lives here
-import "../styles/style.css";    // (optional base styles)
-
-/** Keep _app.js minimal: just pass page props */
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
