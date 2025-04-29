9e6374fa6e0675242d2d23a8a2918fb7178a43b4
jhyj92 <bloodpiru@gmail.com>
2025-04-29 22:55:14 +1000

    Create conversationScript.js

---
+++ utils/conversationScript.js
@@ -0,0 +1,12 @@
+export const scripts={
+  dreamweaver:{
+    intro:["You walk a path drawn in starlight. Few make it here, fewer still return unchanged."],
+    choices:[
+      "I came looking for something I lost.",
+      "I’m not sure why I came.",
+      "I thought this was just a dream."
+    ],
+    followup:["Even dreams have tides. You bring more with you than you know."]
+  },
+  strategist:{ /* add later */ }
+};
