a05bb34389937cc756930bd7549dd1cf418c77fd
jhyj92 <bloodpiru@gmail.com>
2025-04-30 00:32:01 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+// pages/_app.js
+
 import "../styles/starfield.css";
+
 export default function App({ Component, pageProps }) {
+  // Wrap in starfield and allow theme class on <main>
   return (
     <>
       <div className="starfield" />
