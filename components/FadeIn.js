110c8465d198fb41d119a560e44e465eeb627272
jhyj92 <bloodpiru@gmail.com>
2025-05-01 00:19:26 +1000

    Delete components/FadeIn.js

--- components/FadeIn.js
+++
@@ -1,9 +0,0 @@
-// components/FadeIn.js
-
-export default function FadeIn({ children, delay = 0 }) {
-  return (
-    <div style={{ animation: `fade 1s ease ${delay}s both` }}>
-      {children}
-    </div>
-  );
-}
