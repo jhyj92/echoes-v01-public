60370cf0f4ad57a1b9b00fa5e90171915f321e72
jhyj92 <bloodpiru@gmail.com>
2025-04-30 22:22:50 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{tsx,ts}", "./components/**/*.{tsx,ts}"],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
-      colors: { gold: "#C8AE7D" },
+      colors: { gold: "#c8ae7d" },
       keyframes: {
-        star: { "0%": { backgroundPosition: "0 0" }, "100%": { backgroundPosition: "1000px 1000px" } },
+        star: {
+          "0%":   { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "1000px 1000px" },
+        },
       },
-      animation: { starfield: "star 60s linear infinite" },
+      animation: {
+        starfield: "star 60s linear infinite",
+      },
     },
   },
   plugins: [],
