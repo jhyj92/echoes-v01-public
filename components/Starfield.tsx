c6902056dddb86fb46637bea61cf10ad0af82293
jhyj92 <bloodpiru@gmail.com>
2025-04-30 22:09:31 +1000

    Create Starfield.tsx

---
+++ components/Starfield.tsx
@@ -0,0 +1,5 @@
+export default function Starfield() {
+  return (
+    <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.2)_1px,transparent_1px)] [background-size:4px_4px] animate-starfield" />
+  );
+}
