70dfad9a812064b35aaafde80fe4278044deee76
jhyj92 <bloodpiru@gmail.com>
2025-05-03 01:40:22 +1000

    Update guide.tsx

--- pages/guide.tsx
+++ pages/guide.tsx
@@ -48,6 +48,6 @@ export default function GuidePage() {
       {!loading && selectedScenario && (
         <HeroChat scenario={selectedScenario} />
       )}
-    </main>  {/* ✅ THIS WAS MISSING */}
+    </main> {/* ✅ THIS is correct */}
   );
 }
