36ff84b58c599aa48a2dcec7e21f36e80a53cffd
jhyj92 <bloodpiru@gmail.com>
2025-05-03 16:16:23 +1000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,18 @@
 // pages/_document.tsx
-import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body className="bg-black text-gold">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en" className="bg-black text-gold">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body className="bg-black text-gold">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
