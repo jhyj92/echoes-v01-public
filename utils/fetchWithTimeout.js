6510cccdc9a2645a42b28cfb9d4d95eaa8a08b90
jhyj92 <bloodpiru@gmail.com>
2025-05-03 16:43:44 +1000

    Update fetchWithTimeout.js

--- utils/fetchWithTimeout.js
+++ utils/fetchWithTimeout.js
@@ -1,13 +1,13 @@
 // utils/fetchWithTimeout.js
+
 export default async function fetchWithTimeout(url, options = {}, ms = 10000) {
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), ms);
+
   try {
     const response = await fetch(url, { ...options, signal: controller.signal });
-    clearTimeout(id);
     return response;
-  } catch (err) {
+  } finally {
     clearTimeout(id);
-    throw err;
   }
 }
